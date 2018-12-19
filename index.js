b1d9ba582f5fd2eba4d11491bca43267a5ddd2f5
Christina Burchette <burchc426@gmail.com>
2018-12-19 23:40:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Write your code in this file!
+const someValue = x
+function scuberGreetingForFeet(someValue) {
+  if (someValue <=400) {return "This one is on me!"
+}
+  else if (someValue >=2000) {
+    return ("I will gladly take your thirty bucks.")}
+  
+  else if (someValue >=2500) 
+      {return ("No can do.")  
+  }
+}
+
+function ternaryCheckCity() 
+{ let response
+return (someCity = "NYC" ? (response = "Ok, sounds good.") : (response = "No go."));
+  
+}
+
+function switchOnCharmFromTip(){
+  let response;
+  switch(tip) {
+    case ("not as generous"):
+      response ="Thank you.";
+      break;
+    case ("generous"):
+      response ="Thank you so much.";
+      break;
+    case ("thanks for everything"): 
+      response ="Bye.";
+  }
+}
