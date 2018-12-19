52afa66849b83cadb6184b5d2dbbc254736ae7bd
Christina Burchette <burchc426@gmail.com>
2018-12-19 23:57:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const someValue = x
 function scuberGreetingForFeet(someValue) {
   if (someValue <=400) {return "This one is on me!"
 }
+  else if (someValue >=2500) 
+      {return ("No can do.")  }
+      
   else if (someValue >=2000) {
     return ("I will gladly take your thirty bucks.")}
-  
-  else if (someValue >=2500) 
-      {return ("No can do.")  
   }
 }
 
